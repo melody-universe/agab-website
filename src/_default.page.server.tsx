7c0bf8040e269f5aaf39e61ed5dd8685965db4a3
Melody Universe <melody.universe.2021@gmail.com>
2023-08-24 14:19:39 -0700

    created basic build

--- src/_default.page.server.tsx
+++ src/_default.page.server.tsx
@@ -1,15 +1,15 @@
 import { Eta } from "eta";
-import { fileURLToPath } from "url";
 import ReactDOMServer from "react-dom/server";
 import { dangerouslySkipEscape } from "vite-plugin-ssr/server";
+import template from "./index.eta?raw";
 import { PageContext } from "./types";
 
 export const passToClient = ["documentProps", "pageProps"];
 
-const eta = new Eta({ views: fileURLToPath(new URL(".", import.meta.url)) });
+const eta = new Eta();
 export const render = ({ exports, Page, pageProps }: PageContext) => ({
   documentHtml: dangerouslySkipEscape(
-    eta.render("./index", {
+    eta.renderString(template, {
       contents: ReactDOMServer.renderToString(<Page {...pageProps} />),
       title: exports.title ?? "AGAB",
     })

ea513e2a9959dde80aec81f92c45f003462afdf5
Melody Universe <melody.universe.2021@gmail.com>
2023-08-24 15:54:59 -0700

    removed console log from testing

--- src/setViewHeight.ts
+++ src/setViewHeight.ts
@@ -2,7 +2,6 @@ setViewHeight();
 window.addEventListener("resize", setViewHeight);
 
 function setViewHeight() {
-  console.log("test");
   document.documentElement.style.setProperty(
     "--vh",
     `${window.innerHeight * 0.01}px`

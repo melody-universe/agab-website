ddcb9c578442c7e47ffbe4128490534e1d2b0c0b
Melody Universe <melody.universe.2021@gmail.com>
2023-08-24 23:17:46 -0700

    added logo

--- src/index.page.tsx
+++ src/index.page.tsx
@@ -1,5 +1,17 @@
+import { ReactComponent as AgabLogo } from "./assets/agab.svg";
+
 export const Page = () => (
   <>
+    <AgabLogo
+      title="AGAB"
+      preserveAspectRatio="xMidYMid meet"
+      style={{
+        minHeight: "10em",
+        maxHeight: "20em",
+        minWidth: "50vw",
+        maxWidth: "80vw",
+      }}
+    />
     <h1>Assigned Gay At Band</h1>
     <a href="mailto:contact@aga.band">Contact</a>
   </>

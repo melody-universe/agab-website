2d873b5c2b25ee9f330b0c35e3bc5146dce7275d
Melody Universe <melody.universe.2021@gmail.com>
2023-08-26 11:22:25 -0700

    test out alt banner

--- src/index.page.tsx
+++ src/index.page.tsx
@@ -1,22 +1,34 @@
 import BandName from "./BandName";
-import { ReactComponent as AgabLogo } from "./assets/agab.svg";
+import { ReactComponent as BoldLogo } from "./assets/agab.svg";
+import { ReactComponent as LazyDogLogo } from "./assets/agab-alt.svg";
+import { ComponentProps, useCallback, useState } from "react";
 
-export const Page = () => (
-  <>
-    <AgabLogo
-      title="AGAB"
-      preserveAspectRatio="xMidYMid meet"
-      style={{
-        minHeight: "10em",
-        maxHeight: "20em",
-        minWidth: "50vw",
-        maxWidth: "80vw",
-        userSelect: "none",
-      }}
-    />
-    <BandName />
-    <p>
-      <a href="mailto:contact@aga.band">Contact</a>
-    </p>
-  </>
-);
+export const Page = () => {
+  const [alt, setAlt] = useState(false);
+  const toggleAlt = useCallback(() => setAlt(!alt), [alt]);
+  const bannerProps: SvgProps = {
+    onClick: toggleAlt,
+    preserveAspectRatio: "xMidYMid meet",
+    style: {
+      cursor: "pointer",
+      minHeight: "10em",
+      maxHeight: "20em",
+      minWidth: "50vw",
+      maxWidth: "80vw",
+      userSelect: "none",
+    },
+    title: "AGAB",
+  };
+
+  return (
+    <>
+      {alt ? <LazyDogLogo {...bannerProps} /> : <BoldLogo {...bannerProps} />}
+      <BandName />
+      <p>
+        <a href="mailto:contact@aga.band">Contact</a>
+      </p>
+    </>
+  );
+};
+
+type SvgProps = ComponentProps<"svg"> & { title?: string };

db031692e866ad0dcfb460eede383b171fc0e91e
Melody Universe <melody.universe.2021@gmail.com>
2023-10-18 14:44:42 -0700

    added band description and removed togglable logo

--- src/index.page.tsx
+++ src/index.page.tsx
@@ -1,34 +1,26 @@
 import BandName from "./BandName";
-import { ReactComponent as BoldLogo } from "./assets/agab.svg";
-import { ReactComponent as LazyDogLogo } from "./assets/agab-alt.svg";
-import { ComponentProps, useCallback, useState } from "react";
+import { ReactComponent as Logo } from "./assets/agab.svg";
 
-export const Page = () => {
-  const [alt, setAlt] = useState(false);
-  const toggleAlt = useCallback(() => setAlt(!alt), [alt]);
-  const bannerProps: SvgProps = {
-    onClick: toggleAlt,
-    preserveAspectRatio: "xMidYMid meet",
-    style: {
-      cursor: "pointer",
-      minHeight: "10em",
-      maxHeight: "16em",
-      minWidth: "50vw",
-      maxWidth: "80vw",
-      userSelect: "none",
-    },
-    title: "AGAB",
-  };
-
-  return (
-    <>
-      {alt ? <LazyDogLogo {...bannerProps} /> : <BoldLogo {...bannerProps} />}
-      <BandName />
-      <p>
-        <a href="mailto:contact@aga.band">Contact</a>
-      </p>
-    </>
-  );
-};
-
-type SvgProps = ComponentProps<"svg"> & { title?: string };
+export const Page = () => (
+  <>
+    <Logo
+      preserveAspectRatio="xMidYMid meet"
+      style={{
+        minHeight: "10em",
+        maxHeight: "16em",
+        minWidth: "50vw",
+        maxWidth: "80vw",
+      }}
+      title="AGAB"
+    />
+    <p>
+      A queer joy rebellion playing brass bangers for the theys and gays of
+      every generation in Seattle and beyond.
+    </p>
+    <p>What does AGAB mean? Well, it stands for:</p>
+    <BandName />
+    <p>
+      <a href="mailto:contact@aga.band">Contact</a>
+    </p>
+  </>
+);

dc3b12840599b9a123df17c39e416538bc2c8d1b
Melody Universe <them@melody-universe.com>
2026-04-04 22:37:40 -0700

    create non-functional register page

--- src/app/components/Textbox.tsx
+++ src/app/components/Textbox.tsx
@@ -2,9 +2,21 @@ import "./Textbox.scss";
 
 import { ComponentChild } from "preact";
 import type { InputHTMLAttributes } from "preact";
+import { useEffect, useRef } from "preact/hooks";
 
-export function Textbox({ type, ...props }: TextboxProps): ComponentChild {
-  return <input {...{ type, ...props }} className="textbox" />;
+export function Textbox({
+  autoFocus,
+  type,
+  ...props
+}: TextboxProps): ComponentChild {
+  const ref = useRef<HTMLInputElement>(null);
+  useEffect(() => {
+    if (autoFocus) {
+      ref.current?.focus();
+    }
+  }, [autoFocus]);
+
+  return <input {...{ type, ...props }} className="textbox" ref={ref} />;
 }
 
 type TextboxProps = {
--- src/app/routes/admin/register.tsx
+++ src/app/routes/admin/register.tsx
@@ -1,5 +1,7 @@
 import { ComponentChild } from "preact";
 import { Card } from "../../components/Card";
+import { Form } from "../../components/Form";
+import { Textbox } from "../../components/Textbox";
 
 export function loader() {}
 
@@ -7,7 +9,23 @@ export function Page(): ComponentChild {
   return (
     <Card>
       <h1>Register</h1>
-      <form></form>
+      <Form>
+        <Textbox
+          type="text"
+          placeholder="Activation code"
+          autoFocus
+          tabIndex={1}
+        />
+        <Textbox type="text" placeholder="Username" tabIndex={2} />
+        <Textbox type="password" placeholder="Password" tabIndex={3} />
+        <Textbox type="password" placeholder="Confirm password" tabIndex={4} />
+        <div class="submit-row">
+          <a tabIndex={6} href="./login">
+            Login
+          </a>
+          <input tabIndex={5} type="submit" value="Go" />
+        </div>
+      </Form>
     </Card>
   );
 }

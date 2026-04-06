9ba71fc5de6caac6da9adddabd09d314c71d7306
Melody Universe <them@melody-universe.com>
2026-04-06 12:43:06 -0700

    require auth for admins

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,46 @@ import { Hono } from "hono";
 import api from "./api";
 import { routes } from "./routes";
 import { ssrWithLoader } from "./lib/ssr";
+import { deleteCookie, getCookie } from "hono/cookie";
+import { validateSessionToken } from "./api/sessions";
 
 const app = new Hono<{ Bindings: Env }>();
 
 app.route("/api", api);
+
+app.use("/admin/*", async (c, next) => {
+  const token = getCookie(c, "session");
+
+  if (!token) {
+    if (isOnAuthPage()) {
+      return next();
+    }
+
+    return c.redirect("/admin/login");
+  }
+
+  const sessionValidation = await validateSessionToken(c, token);
+
+  if (!sessionValidation.isSuccess) {
+    deleteCookie(c, "session");
+
+    if (isOnAuthPage()) {
+      return next();
+    }
+
+    return c.redirect("/admin/login");
+  }
+
+  if (isOnAuthPage()) {
+    return c.redirect("/admin");
+  }
+
+  return next();
+
+  function isOnAuthPage() {
+    return ["/admin/login", "/admin/register"].includes(c.req.path);
+  }
+});
 
 routes.forEach((route) => {
   // @ts-expect-error TODO: Improve type safety of routes.

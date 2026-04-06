23137e96fbfe05c7b3e59a4fcff24fd9e570c95f
Melody Universe <them@melody-universe.com>
2026-04-06 11:01:16 -0700

    implement validateSessionToken

--- src/api/sessions.ts
+++ src/api/sessions.ts
@@ -3,10 +3,11 @@ import {
   encodeBase32LowerCaseNoPadding,
   encodeHexLowerCase,
 } from "@oslojs/encoding";
-import { Session, sessions, User } from "../db/schema";
+import { Session, sessions, User, users } from "../db/schema";
 import { sha256 } from "@oslojs/crypto/sha2";
 import { drizzle } from "drizzle-orm/d1";
 import { Context } from "hono";
+import { eq } from "drizzle-orm";
 
 export function generateSessionToken(): string {
   const bytes = new Uint8Array(20);
@@ -34,8 +35,38 @@ export async function createSession(
 }
 
 export async function validateSessionToken(
+  c: Context<{ Bindings: Env }>,
   token: string,
-): Promise<SessionValidationResult> {}
+): Promise<SessionValidationResult> {
+  const db = drizzle(c.env.DB);
+
+  const sessionId = encodeHexLowerCase(sha256(new TextEncoder().encode(token)));
+  const result = await db
+    .select({ user: users, session: sessions })
+    .from(sessions)
+    .innerJoin(users, eq(sessions.userId, users.id))
+    .where(eq(sessions.id, sessionId));
+
+  if (result.length === 0) {
+    return { isSuccess: false };
+  }
+
+  const { session, user } = result[0];
+  if (Date.now() >= session.expiresAt.getTime()) {
+    await db.delete(sessions).where(eq(sessions.id, sessionId));
+    return { isSuccess: false };
+  }
+
+  if (Date.now() >= session.expiresAt.getTime() - 1000 * 60 * 60 * 24 * 15) {
+    session.expiresAt = new Date(Date.now() + 1000 * 60 * 60 * 24 * 30);
+    await db
+      .update(sessions)
+      .set({ expiresAt: session.expiresAt })
+      .where(eq(sessions.id, session.id));
+  }
+
+  return { isSuccess: true, session, user };
+}
 
 export async function invalidateSession(sessionId: string): Promise<void> {}
 

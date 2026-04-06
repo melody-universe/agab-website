65f885b868da02a74b54742c2540111a86f79eab
Melody Universe <them@melody-universe.com>
2026-04-06 10:50:25 -0700

    generate session token

--- src/api/sessions.ts
+++ src/api/sessions.ts
@@ -1,6 +1,13 @@
+import { crypto } from "@cloudflare/workers-types/experimental";
+import { encodeBase32LowerCaseNoPadding } from "@oslojs/encoding";
 import { Session, User } from "../db/schema";
 
-export function generateSessionToken(): string {}
+export function generateSessionToken(): string {
+  const bytes = new Uint8Array(20);
+  crypto.getRandomValues(bytes);
+  const token = encodeBase32LowerCaseNoPadding(bytes);
+  return token;
+}
 
 export async function createSession(
   token: string,

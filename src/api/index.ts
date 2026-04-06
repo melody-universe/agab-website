48e26236df76763577290ebe0e09679e336d1449
Melody Universe <them@melody-universe.com>
2026-04-06 11:10:29 -0700

    use csrf

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,8 +1,10 @@
 import { Hono } from "hono";
 import { getAcronyms } from "./acronyms";
 import { register } from "./register";
+import { csrf } from "hono/csrf";
 
 const api = new Hono<{ Bindings: Env }>()
+  .use(csrf())
   .get("/acronyms", async (c) => c.json(await getAcronyms(c)))
   .post("/register", async (c) => c.json(await register(c)));
 

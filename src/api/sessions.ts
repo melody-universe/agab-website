110f7d9c003dd5f2e8064291c3de539eca8b5f5a
Melody Universe <them@melody-universe.com>
2026-04-06 10:54:24 -0700

    create session

--- src/api/sessions.ts
+++ src/api/sessions.ts
@@ -1,6 +1,12 @@
 import { crypto } from "@cloudflare/workers-types/experimental";
-import { encodeBase32LowerCaseNoPadding } from "@oslojs/encoding";
-import { Session, User } from "../db/schema";
+import {
+  encodeBase32LowerCaseNoPadding,
+  encodeHexLowerCase,
+} from "@oslojs/encoding";
+import { Session, sessions, User } from "../db/schema";
+import { sha256 } from "@oslojs/crypto/sha2";
+import { drizzle } from "drizzle-orm/d1";
+import { Context } from "hono";
 
 export function generateSessionToken(): string {
   const bytes = new Uint8Array(20);
@@ -10,9 +16,22 @@ export function generateSessionToken(): string {
 }
 
 export async function createSession(
+  c: Context<{ Bindings: Env }>,
   token: string,
   userId: number,
-): Promise<Session> {}
+): Promise<Session> {
+  const sessionId = encodeHexLowerCase(sha256(new TextEncoder().encode(token)));
+  const session: Session = {
+    id: sessionId,
+    userId,
+    expiresAt: new Date(Date.now() + 1000 * 60 * 60 * 24 * 30),
+  };
+
+  const db = drizzle(c.env.DB);
+  await db.insert(sessions).values(session);
+
+  return session;
+}
 
 export async function validateSessionToken(
   token: string,

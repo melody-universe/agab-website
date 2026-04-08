d1dcf7f24a87dc5a9bd934ab475bbaa9b59aebda
Melody Universe <them@melody-universe.com>
2026-04-07 23:05:20 -0700

    turn card into a module

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -1,9 +1,9 @@
-import "./Card.scss";
+import styles from "./Card.module.scss";
 
 import { ComponentChild, ComponentChildren } from "preact";
 
 export function Card({ children }: CardProps): ComponentChild {
-  return <div className="card">{children}</div>;
+  return <div className={styles.card}>{children}</div>;
 }
 
 type CardProps = { children?: ComponentChildren };
